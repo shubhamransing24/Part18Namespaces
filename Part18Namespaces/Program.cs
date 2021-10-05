191fb26f992a30646a38fc55382d813dea568bd4
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-05 17:57:38 +0530

    Add project files.

---
+++ Part18Namespaces/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Shu=Shubham.A;
+using Sau=Saurabh.A;
+namespace Part18Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////use fully qualified name or use the namespace in top
+            //Shubham.A.print();
+            //Saurabh.B.print();
+
+            Shu.A.print();
+            Sau.B.print();
+        }
+    }
+}
+namespace Shubham.A
+{
+    class A
+    {
+        public static void print()
+        {
+            Console.WriteLine("Print Method of Class A");
+
+        }
+    }
+}
+namespace Saurabh.A
+{
+    class B
+    {
+        public static void print()
+        {
+            Console.WriteLine("Print Method of Class B");
+
+        }
+    }
+}
